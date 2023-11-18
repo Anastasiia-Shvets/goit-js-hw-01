83486f2159c09d323bb609fd0c3c4402334352c8
Anastasiia-Shvets <144601223+Anastasiia-Shvets@users.noreply.github.com>
2023-11-18 18:30:53 +0200

    finishCorection

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 'use strict';
 function getElementWidth(content, padding, border) {
-    let num = Number.parseFloat(content) + 2 * (Number.parseFloat(padding) + Number.parseFloat(border));
-    return num;
+    let totalWidth = Number.parseFloat(content) + 2 * (Number.parseFloat(padding) + Number.parseFloat(border));
+    return totalWidth;
 }
 console.log(getElementWidth('50px', '8px', '4px'));
 console.log(getElementWidth('60px', '12px', '8.5px'));
