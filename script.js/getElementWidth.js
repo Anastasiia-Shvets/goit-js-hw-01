8b5f1827982e260728496209357d6d4031deb74d
Anastasiia-Shvets <144601223+Anastasiia-Shvets@users.noreply.github.com>
2023-11-18 17:25:17 +0200

    js

--- script.js/getElementWidth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function getElementWidth(content, padding, border) {
-  content = Number(content);
-  padding = Number(padding);
-  border = Number(border);
-  const perimet = content + 2 * (padding + border);
-  console.log(perimet);
-}
